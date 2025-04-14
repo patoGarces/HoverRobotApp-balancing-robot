548a0cc68b195840add758874c69eaf8c1a798c4
PatricioGarces <patricio.garces@outlook.com>
2025-04-14 00:52:21 -0300

    Actualizo version a 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.app.hoverrobot"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "2.0.0"
+        versionCode = 2
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
